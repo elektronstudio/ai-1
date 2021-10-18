c4c78c117fa6f7f6e05ae0a4f7645d790b8cecd1
kristjanjansen <kristjan.jansen@gmail.com>
2021-10-18 08:48:08 +0200

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,12 @@
-<script setup lang="ts">
-//@ts-nocheck
-import { ref, onMounted } from "vue";
-import "@tensorflow/tfjs-backend-cpu";
-import "@tensorflow/tfjs-backend-webgl";
-import * as cocoSsd from "@tensorflow-models/coco-ssd";
-
-const videoRef = ref<HTMLVideoElement | null>(null);
-const canvasRef = ref<HTMLCanvasElement | null>(null);
-const predictions = ref<cocoSsd.DetectedObject[]>([]);
-
-onMounted(async () => {
-  const videoStream = await navigator.mediaDevices.getUserMedia({
-    video: {},
-  });
-  videoRef.value.srcObject = videoStream;
-  const ctx = canvasRef.value.getContext("2d");
-
-  const model = await cocoSsd.load();
-
-  //videoRef.value.addEventListener("play", async () => {
-  async function step() {
-    predictions.value = await model.detect(videoRef.value);
-    ctx.drawImage(videoRef.value, 0, 0, 640, 480);
-    for (let i = 0; i < predictions.value.length; i++) {
-      ctx.beginPath();
-      ctx.rect(...predictions.value[i].bbox);
-      ctx.lineWidth = 1;
-      ctx.strokeStyle = "green";
-      ctx.fillStyle = "green";
-      ctx.stroke();
-      ctx.fillText(
-        predictions.value[i].score.toFixed(3) +
-          " " +
-          predictions.value[i].class,
-        predictions.value[i].bbox[0],
-        predictions.value[i].bbox[1] > 10
-          ? predictions.value[i].bbox[1] - 5
-          : 10
-      );
-    }
-    requestAnimationFrame(step);
-  }
-  requestAnimationFrame(step);
-});
-</script>
-
 <template>
-  <div>Hello world</div>
-  <pre>{{ predictions }}</pre>
-  <canvas ref="canvasRef" width="640" height="480"></canvas>
-  <video ref="videoRef" autoplay />
+  <div>
+    <div style="display: flex; margin-bottom: 16px; gap: 16px">
+      <RouterLink to="/">CocoSSD object detection</RouterLink>
+      <RouterLink to="/app2">Human.js human detection</RouterLink>
+      <RouterLink to="/app3">TF Holistic human detection</RouterLink>
+    </div>
+    <RouterView v-slot="{ Component }">
+      <component :is="Component" />
+    </RouterView>
+  </div>
 </template>
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,29 @@
 import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+
 import App from "./App.vue";
 import "./index.css";
 
-createApp(App).mount("#app");
+const routes = [
+  {
+    path: "/",
+    component: () => import(`./pages/App1.vue`),
+  },
+  {
+    path: "/app2",
+    component: () => import(`./pages/App2.vue`),
+  },
+  {
+    path: "/app3",
+    component: () => import(`./pages/App3.vue`),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
