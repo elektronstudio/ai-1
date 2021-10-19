1627d0f15673f08f513c532106aa10ec70c93c8a
kristjanjansen <kristjan.jansen@gmail.com>
2021-10-19 17:41:28 +0200

    cleanup

--- src/pages/Objects3.vue
+++ src/pages/Objects3.vue
@@ -6,6 +6,7 @@ import "@tensorflow/tfjs-backend-webgl";
 import * as cocoSsd from "@tensorflow-models/coco-ssd";
 import { useRafFn } from "@vueuse/core";
 import { buffer } from "@tensorflow/tfjs-core";
+import standardDeviation from "just-standard-deviation";
 
 const videoRef = ref<HTMLVideoElement | null>(null);
 const canvasRef = ref<HTMLCanvasElement | null>(null);
@@ -13,11 +14,23 @@ const predictions = ref<cocoSsd.DetectedObject[] | null>(null);
 const width = ref(0);
 const height = ref(0);
 
+const objects = ref([]);
+
 function center(bbox) {
   const cX = bbox[2] / 2 + bbox[0];
   const cY = bbox[3] / 2 + bbox[1];
   return [cX, cY];
 }
+
+const distance = (x1, y1, x2, y2) =>
+  Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+const linepoint = (x1, y1, x2, y2, d) => {
+  const dis = distance(x1, y1, x2, y2);
+  const x3 = x1 + (d / dis) * (x2 - x1);
+  const y3 = y1 + (d / dis) * (y2 - y1);
+  return [x3, y3];
+};
 
 function pointInsideCircle(x, y, cx, cy, radius) {
   const distancesquared = (x - cx) * (x - cx) + (y - cy) * (y - cy);
@@ -25,6 +38,8 @@ function pointInsideCircle(x, y, cx, cy, radius) {
 }
 
 const buf = ref([]);
+const status = ref("");
+const playing = ref(false);
 
 onMounted(async () => {
   // const devices = await navigator.mediaDevices.enumerateDevices();
@@ -35,11 +50,30 @@ onMounted(async () => {
   //       "4e51f67844f0d40aa0a002fe8d8413faf5230dd328d8235f6b9b87d9ad9dfb1c",
   //   },
   // });
-  //videoRef.value.srcObject = videoStream;
+  // videoRef.value.srcObject = videoStream;
 
   videoRef.value.addEventListener("loadedmetadata", (e) => {
     width.value = e.target.videoWidth;
     height.value = e.target.videoHeight;
+  });
+
+  videoRef.value.addEventListener("playing", (e) => {
+    playing.value = true;
+  });
+
+  videoRef.value.addEventListener("seeking", (e) => {
+    playing.value = false;
+  });
+
+  const statuses =
+    "abort canplay canplaythrough durationchange emptied encrypted ended error interruptbegin interruptend loadeddata loadedmetadata loadstart mozaudioavailable pause play playing progress ratechange seeked seeking stalled suspend timeupdate volumechange waiting".split(
+      " "
+    );
+
+  statuses.forEach((s) => {
+    videoRef.value.addEventListener(s, (e) => {
+      console.log(s);
+    });
   });
 
   const ctx = canvasRef.value.getContext("2d");
@@ -49,11 +83,11 @@ onMounted(async () => {
   let count = 0;
   let prevCenter = [0, 0];
   let drawableCenter = [0, 0];
-  let objects = [];
   const limit = 60;
-  const bufferSize = 50;
+  const bufferSize = 10;
+
   useRafFn(async () => {
-    if (width.value) {
+    if (playing.value) {
       predictions.value = await model.detect(videoRef.value, 100, 0.5);
 
       predictions.value
@@ -63,18 +97,24 @@ onMounted(async () => {
         })
         .filter((p) => p.class === "person")
         .forEach((p, i) => {
-          const objectIndex = objects.findIndex((o) =>
+          const objectIndex = objects.value.findIndex((o) =>
             pointInsideCircle(...p.center, ...o.center, 150)
           );
           if (objectIndex > -1) {
-            objects[objectIndex].currentCenter = p.center;
-            if (objects[objectIndex].buffer.length > bufferSize - 1) {
-              objects[objectIndex].buffer.shift();
+            objects.value[objectIndex].currentCenter = p.center;
+            if (objects.value[objectIndex].buffer.length > bufferSize - 1) {
+              objects.value[objectIndex].buffer.shift();
             }
-            objects[objectIndex].buffer.push(p.center);
+            objects.value[objectIndex].buffer.push(p.center);
+            objects.value[objectIndex].xDev = standardDeviation(
+              objects.value[objectIndex].buffer.map(([x, y]) => x)
+            );
+            objects.value[objectIndex].yDev = standardDeviation(
+              objects.value[objectIndex].buffer.map(([x, y]) => y)
+            );
           } else {
-            objects.forEach((_, i) => (objects[i].updated = false));
-            objects.push({
+            objects.value.forEach((_, i) => (objects.value[i].updated = false));
+            objects.value.push({
               ...p,
               currentCenter: p.center,
               updated: true,
@@ -86,7 +126,7 @@ onMounted(async () => {
 
       ctx.drawImage(videoRef.value, 0, 0, width.value, height.value);
       ctx.font = "20px Arial";
-      objects.forEach((o, i) => {
+      objects.value.forEach((o, i) => {
         o.buffer.forEach((b, j) => {
           ctx.fillStyle = `hsl(${i * 30},100%,50%,0.05)`;
           ctx.beginPath();
@@ -102,7 +142,7 @@ onMounted(async () => {
       });
 
       if (count > limit) {
-        //objects = [];
+        //objects.value = [];
         count = 0;
       } else {
         count++;
@@ -151,23 +191,23 @@ onMounted(async () => {
 
 <template>
   <div style="transform: scale(1)">
+    <p>Status: {{ playing ? "Playing" : "Not playing" }}</p>
     <p v-if="!predictions">Loading...</p>
-    <div style="position: relative">
-      <video
-        ref="videoRef"
-        autoplay
-        muted
-        loop
-        src="/sample2.mp4"
-        style="position: absolute; top: 0; left: 0; width: 100vw"
-      />
-      <canvas
-        ref="canvasRef"
-        :width="width"
-        :height="height"
-        style="position: absolute; top: 0; left: 0; width: 100vw"
-      ></canvas>
-    </div>
+    <canvas
+      ref="canvasRef"
+      :width="width"
+      :height="height"
+      style="width: 100vw"
+    ></canvas>
+    <video
+      ref="videoRef"
+      autoplay
+      muted
+      loop
+      src="/sample2.mp4"
+      style="width: 100vw"
+    />
     <!-- <video ref="videoRef" autoplay muted loop style="width: 100vw" /> -->
+    <pre>{{ objects }}</pre>
   </div>
 </template>
