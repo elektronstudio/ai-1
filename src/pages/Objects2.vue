0adedc08de4689e0bf0f6e70aa83881f2abdf792
kristjanjansen <kristjan.jansen@gmail.com>
2021-10-19 12:25:13 +0200

    cleanup

--- src/pages/Objects2.vue
+++ src/pages/Objects2.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
+//@ts-nocheck
 import * as Tone from "tone";
 import { ref } from "vue";
+import "@tensorflow/tfjs-backend-cpu";
+import "@tensorflow/tfjs-backend-webgl";
+import * as cocoSsd from "@tensorflow-models/coco-ssd";
 
 const frame = ref(0);
 
-const fps = 60;
-const musicFps = 1;
+const fps = 10;
+const videoFps = 2;
 const length = 5;
 let values = [];
 
@@ -24,9 +28,20 @@ const predictions = ref<cocoSsd.DetectedObject[] | null>(null);
 const width = ref(0);
 const height = ref(0);
 
+function center(bbox) {
+  const cX = bbox[2] / 2 + bbox[0];
+  const cY = bbox[3] / 2 + bbox[1];
+  return [cX, cY];
+}
+
+function pointInsideCircle(x, y, cx, cy, radius) {
+  const distancesquared = (x - cx) * (x - cx) + (y - cy) * (y - cy);
+  return distancesquared <= radius * radius;
+}
+
 const onStart = async () => {
+  const model = await cocoSsd.load();
   const devices = await navigator.mediaDevices.enumerateDevices();
-  console.log(devices);
   const videoStream = await navigator.mediaDevices.getUserMedia({
     video: {
       deviceId:
@@ -35,7 +50,7 @@ const onStart = async () => {
   });
   if (videoRef.value && canvasRef.value) {
     videoRef.value.srcObject = videoStream;
-    videoRef.value.addEventListener("loadedmetadata", (e) => {
+    videoRef.value.addEventListener("loadeddata", (e) => {
       width.value = e?.target?.videoWidth;
       height.value = e?.target?.videoHeight;
     });
@@ -43,29 +58,81 @@ const onStart = async () => {
 
   const ctx = canvasRef.value.getContext("2d");
 
+  let count = 0;
+  let prevCenter = [0, 0];
+  let drawableCenter = [0, 0];
+  let objects = [];
+  const limit = 5;
+
   Tone.start().then(() => {
     new Tone.Loop((time) => {
-      Tone.Draw.schedule(() => {
+      Tone.Draw.schedule(async () => {
         frame.value = Math.floor((time * fps) % fps);
 
         // Video
 
-        ctx.drawImage(videoRef.value, 0, 0, width.value, height.value);
+        if (frame.value % videoFps === 0) {
+          if (width.value) {
+            predictions.value = await model.detect(videoRef.value, 100, 0.5);
+            ctx.drawImage(videoRef.value, 0, 0, width.value, height.value);
 
+            predictions.value
+              .map((p) => {
+                p.center = center(p.bbox);
+                return p;
+              })
+              .filter((p) => p.class === "person")
+              .forEach((p, i) => {
+                const objectIndex = objects.findIndex((o) =>
+                  pointInsideCircle(...p.center, ...o.center, 100)
+                );
+                if (objectIndex > -1) {
+                  objects[objectIndex].currentCenter = p.center;
+                } else {
+                  objects.forEach((_, i) => (objects[i].updated = false));
+                  objects.push({
+                    ...p,
+                    currentCenter: p.center,
+                    updated: true,
+                  });
+                }
+              });
+
+            //
+
+            ctx.font = "20px Arial";
+            ctx.fillStyle = "green";
+
+            objects.forEach((o) => {
+              ctx.beginPath();
+              ctx.lineWidth = 1;
+              ctx.strokeStyle = "green";
+              ctx.arc(...o.center, 100, 0, 2 * Math.PI);
+              ctx.arc(...o.center, 10, 0, 2 * Math.PI);
+              ctx.stroke();
+              ctx.strokeStyle = "red";
+              ctx.arc(...o.currentCenter, 10, 0, 2 * Math.PI);
+              ctx.stroke();
+              ctx.closePath();
+            });
+
+            if (frame.value === limit) {
+              objects = [];
+            }
+          }
+        }
         // Sound
 
-        if (frame.value % musicFps === 0) {
-          values = Array.from({ length }).map((_) => 100 + Math.random() * 10);
-          values.forEach((freq, i) => {
-            synths[i].triggerAttackRelease(freq, 1 / fps);
-          });
-          // if (frame.value === 0) {
-          //   outlierSynth.triggerAttackRelease(
-          //     200 + Math.random() * 200 - 100,
-          //     1 / fps
-          //   );
-          // }
-        }
+        values = Array.from({ length }).map((_) => 100 + Math.random() * 10);
+        values.forEach((freq, i) => {
+          synths[i].triggerAttackRelease(freq, 1 / fps);
+        });
+        // if (frame.value === 0) {
+        //   outlierSynth.triggerAttackRelease(
+        //     200 + Math.random() * 200 - 100,
+        //     1 / fps
+        //   );
+        // }
       }, time);
     }, 1 / fps).start(0);
     Tone.Transport.start();
@@ -76,7 +143,7 @@ const onStart = async () => {
 <template>
   <div>
     <pre>{{ frame }}</pre>
-    <pre>{{ frame % musicFps === 0 ? "beat" : " " }}</pre>
+    <pre>{{ frame % videoFps === 0 ? "beat" : " " }}</pre>
     <button @click="onStart">Start</button>
     <div style="transform: scale(1)">
       <canvas
