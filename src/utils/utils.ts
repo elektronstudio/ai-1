3653f2ae2a23b6eb8edce1e6daf942ce8082821c
kristjanjansen <kristjan.jansen@gmail.com>
2021-10-20 07:55:31 +0200

    clear

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -18,3 +18,13 @@ export function pointInsideCircle(x, y, cx, cy, radius) {
   const distancesquared = (x - cx) * (x - cx) + (y - cy) * (y - cy);
   return distancesquared <= radius * radius;
 }
+
+export function map(
+  value: number,
+  start1: number,
+  stop1: number,
+  start2: number,
+  stop2: number
+): number {
+  return ((value - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
+}
